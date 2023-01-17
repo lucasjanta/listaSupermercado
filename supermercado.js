8be7394c97e6ba4bcb274eae48f979b45fbf19b8
lucasjanta <lucastyle2014@gmail.com>
2023-01-17 17:24:02 -0300

    Começando Javascript

---
+++ supermercado.js
@@ -0,0 +1,7 @@
+var itens = [];
+
+const submitProduto = document.querySelector("name=acao");
+submitProduto.addEventListener('click', ()=>{
+    var nomeProduto = document.querySelector('name=nome_produto');
+    var valorProduto = document.querySelector('name=valor');
+})
